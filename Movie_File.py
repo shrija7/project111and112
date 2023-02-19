89c613b4f60db6e49571d120eb6f4f284173bbbc
shrija7 <98268668+shrija7@users.noreply.github.com>
2023-02-19 18:49:19 +0530

    Add files via upload

---
+++ Movie_File.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+
+# .exe , .msi,  .gif, .png .jpg, .jpeg, .csv, .pdf , .xls , .xlsx , .ppt , .pptx
+
+from_dir = "C:/orange"
+to_dir = "C:/apple"
+
+list_of_files = os.listdir(from_dir)
+# print(list_of_files)
+
+# Move All Image files from Downloads Folder to Another Folder
+for file_name in list_of_files:
+
+    name, extension = os.path.splitext(file_name)
+    #print(name)
+    #print(extension)
+
+    if extension == '':
+        continue
+    if extension in ['.gif', '.png', '.pdf', '.jpeg','.jfif']:
+
+        path1 = from_dir + '/' + file_name                          
+        path2 = to_dir + '/' + "Document_Files"                         
+        path3 = to_dir + '/' + "Document_Files" + '/' + file_name  
+        #print("path1 " , path1)
+        #print("path3 ", path3)
+
+        # Check if Folder/Directory Path Exists Before Moving
+        # Else make a NEW Folder/Directory Then Move
+        
+        if os.path.exists(path2):
+          print("Moving " + file_name + ".....")
+
+          # Move from path1 ---> path3
+          shutil.move(path1, path3)
+
+        else:
+          os.makedirs(path2)
+          print("Moving " + file_name + ".....")
+          shutil.move(path1, path3)
